8f32527a7e86dd4753fe73b8c9580357132930f6
Eric Moyer <eric@lemoncrab.com>
2025-10-19 13:17:59 -0700

    wip:simplepack

--- pkg/simplepack/simplepack.go
+++ pkg/simplepack/simplepack.go
@@ -1,7 +1,62 @@
 package simplepack
 
+import (
+	"app/pkg/quicklog"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 const Version = "2.0.0"
 
 const pathBuild = "project_build"
 const pathEventData = pathBuild + "/app/js/event_data.js"
 const pathTemp = pathBuild + "/temp"
+
+type ReplacementDecriptornt struct {
+	ContentType    string
+	Regex          string
+	ReplacementTag string
+}
+
+var qlog *quicklog.LoggerT = nil // Assigned at runtime
+
+func init() {
+	qlog = quicklog.GetLogger("default")
+}
+
+func SimplePack(pathFileIn string, pathFileOut string, minify bool) error {
+
+	qlog.Infof("SimplePack version: %q", Version)
+	qlog.Infof("    Input file: %q", pathFileIn)
+	qlog.Infof("    Output file: %q", pathFileOut)
+	qlog.Infof("    Minify: %v", minify)
+
+	// Implementation goes here
+	// read file
+	data, err := os.ReadFile(pathFileIn)
+	if err != nil {
+		return fmt.Errorf("error reading input file: %w", err)
+	}
+	lines := strings.Split(string(data), "\n")
+	qlog.Debugf("Read %d lines", len(lines))
+
+	newLines := []string{}
+	for _, line := range lines {
+		pathFileInParentDir := filepath.Dir(pathFileIn)
+		expandedLines, err := expandLine(line, pathFileInParentDir, minify)
+		if err != nil {
+			return fmt.Errorf("error expanding line: %w", err)
+		}
+		for _, expandedLine := range expandedLines {
+			newLines = append(newLines, expandedLine)
+		}
+	}
+	qlog.Infof("Writing: %q", pathFileOut)
+	err = os.WriteFile(pathFileOut, []byte(strings.Join(newLines, "\n")), 0644)
+	if err != nil {
+		return fmt.Errorf("error writing output file: %w", err)
+	}
+	return nil
+}
