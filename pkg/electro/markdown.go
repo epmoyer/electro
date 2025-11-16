dceb9c3fda798c181e42a92a0204f91facc53b1c
Eric Moyer <eric@lemoncrab.com>
2025-11-15 18:15:46 -0800

    wip

--- pkg/electro/markdown.go
+++ pkg/electro/markdown.go
@@ -1,8 +1,14 @@
 package electro
 
 import (
+	"bytes"
 	"fmt"
 	"strings"
+
+	"github.com/yuin/goldmark"
+	highlighting "github.com/yuin/goldmark-highlighting/v2"
+	"github.com/yuin/goldmark/extension"
+	gmhtml "github.com/yuin/goldmark/renderer/html"
 )
 
 type mdRendererT struct {
@@ -29,6 +35,37 @@ func (r *mdRendererT) Render() (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("error pre-parsing markdown content: %w", err)
 	}
+
+	// -------------------------
+	// Render markdown to HTML
+	// -------------------------
+	var bufHtmlBytes bytes.Buffer
+	mdConverter := goldmark.New(
+		goldmark.WithExtensions(
+			extension.GFM,
+			highlighting.NewHighlighting(
+				highlighting.WithStyle("monokai"),
+			),
+		),
+		goldmark.WithRendererOptions(
+			gmhtml.WithUnsafe(),
+		),
+	)
+	err = mdConverter.Convert([]byte(md), &bufHtmlBytes)
+	if err != nil {
+		return "", fmt.Errorf("error converting markdown to HTML: %w", err)
+	}
+	html := bufHtmlBytes.String()
+
+	// -------------------------
+	// Post-parser
+	// -------------------------
+	html, err = r.PostParseHtml(html)
+	if err != nil {
+		return "", fmt.Errorf("error post-processing HTML: %w", err)
+	}
+
+	return html, nil
 }
 
 func (r *mdRendererT) PreParseMarkdown(md string) (string, error) {
@@ -69,4 +106,11 @@ func (r *mdRendererT) stripFrontmatter(md string) (string, error) {
 	}
 
 	return md, fmt.Errorf("frontmatter start found but no closing '---'")
+}
+
+func (r *mdRendererT) PostParseHtml(html string) (string, error) {
+	for placeholder, final := range r.Substitutions {
+		html = strings.ReplaceAll(html, placeholder, final)
+	}
+	return html, nil
 }
